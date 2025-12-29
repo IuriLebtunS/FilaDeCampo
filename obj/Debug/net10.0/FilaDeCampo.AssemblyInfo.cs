ce5e04d1f162960b2a72e1f4221891323ab871c1
Iuri Lebtun Salinas <Lebtuniuri@gmail.com>
2025-12-29 11:47:28 -0300

    apaguei arquivo de mais

--- obj/Debug/net10.0/FilaDeCampo.AssemblyInfo.cs
+++ obj/Debug/net10.0/FilaDeCampo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59ace569b8b16b7c6c36d1880d4431c7610a0c19")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
