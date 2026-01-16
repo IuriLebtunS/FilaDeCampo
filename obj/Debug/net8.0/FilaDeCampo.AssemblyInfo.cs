1b8f92777f4c3af7ce959b9f64b5dba6fd0691c9
Iuri Lebtun Salinas <Lebtuniuri@gmail.com>
2026-01-16 16:47:55 -0300

    adicionei um service

--- obj/Debug/net8.0/FilaDeCampo.AssemblyInfo.cs
+++ obj/Debug/net8.0/FilaDeCampo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e000eba44fbd896b6f13c2baa2ede9f9ee14bb82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88650dcd0580f906b04746108bdf5a6045a3d98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
