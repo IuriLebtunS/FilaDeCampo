2724cab3ae7086867a420b1584c45b90b6dce086
Iuri Lebtun Salinas <Lebtuniuri@gmail.com>
2026-01-16 17:38:42 -0300

    Configuração do projeto e ajustes PostgreSQL

--- obj/Debug/net8.0/FilaDeCampo.AssemblyInfo.cs
+++ obj/Debug/net8.0/FilaDeCampo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a1ef3c2b3d7f403a4419121372d7fb2751e7ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0b28f88eb66ea57a37e16d7a23ac7f7283e0c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilaDeCampo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
